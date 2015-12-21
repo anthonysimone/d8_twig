c93dbe1eeed9f7103ef8e69895a4b2a10806d734
Anthony Simone <anthony.simone7@gmail.com>
2015-12-21 07:07:13 -0700

    Add some styles

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -90,3 +90,17 @@ $settings['extension_discovery_scan_tests'] = TRUE;
  * using these parameters in a request to rebuild.php.
  */
 $settings['rebuild_access'] = TRUE;
+
+/**
+ * Databases
+ */
+//$databases['default']['default'] = array (
+//  'database' => 'd8',
+//  'username' => 'root',
+//  'password' => 'root',
+//  'prefix' => '',
+//  'host' => 'localhost',
+//  'port' => '3306',
+//  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+//  'driver' => 'mysql',
+//);
